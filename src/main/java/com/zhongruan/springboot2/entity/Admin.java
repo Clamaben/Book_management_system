616b6235d2b99cfd802d22cfc75d42cdb0be49e8
chenfeng11111 <1090268270@qq.com>
2019-07-30 10:29:09 +0800

    1

--- src/main/java/com/zhongruan/springboot2/entity/Admin.java
+++ src/main/java/com/zhongruan/springboot2/entity/Admin.java
@@ -1,9 +1,8 @@
 package com.zhongruan.springboot2.entity;
 
 public class Admin extends User {
-    
-    public Admin(String id,String name,String password)
-    {
-        super(id,name,password);
+
+    public Admin(String id, String name, String password) {
+        super(id, name, password);
     }
 }
