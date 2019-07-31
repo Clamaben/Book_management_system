853edf42071c11203dc46fcdb0450b97aec61791
chenfeng11111 <1090268270@qq.com>
2019-07-31 18:13:34 +0800

    登录功能，设置权限

---
+++ src/main/java/com/zhongruan/book_management_system/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.zhongruan.book_management_system.config;
+
+import com.zhongruan.book_management_system.service.Userservice.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    IUserService userService;
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()//配置权限
+                .antMatchers("*.css").permitAll()
+                .antMatchers("/admin/*").hasRole("ADMIN")
+                .antMatchers("/librarian/*").hasRole("LIBRARIAN")
+                .antMatchers("/borrower/*").hasRole("BORROWER")
+                .anyRequest().authenticated()//任意请求需要登录
+                .and()
+                .formLogin()//开启formLogin默认配置
+                .loginPage("/toLogin").permitAll()//请求时未登录跳转接口
+                .failureUrl("/toLoginsadf")//用户密码错误跳转接口
+                .defaultSuccessUrl("/toIndex", true)//登录成功之后的页面
+                .loginProcessingUrl("/login")//处理表单请求的url
+                .usernameParameter("username")//表单提交的参数
+                .passwordParameter("password")
+                .and()
+                .logout()//注销配置
+                .logoutUrl("/logout")//注销页面地址
+                .logoutSuccessUrl("/user/logout")//注销成功后的跳转页面
+                .deleteCookies("myCookie")//删除cookie
+                .and()
+                .csrf()
+                .disable();
+
+
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,38 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Signin Template for Bootstrap</title>
+
+        <link href="/people/signin.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+<c:set var="root" value="${pageContext.request.contextPath}"/>
+<form class="form-signin" action="${root}/login" method="post">
+    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+    <label for="username" class="sr-only">Username</label>
+    <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
+    <label for="password"  class="sr-only">Password</label>
+    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+    <div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Remember me
+        </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
+</form>
+
+
+<!-- 新 Bootstrap4 核心 CSS 文件 -->
+<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
+<script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdn.staticfile.org/popper.js/1.12.5/umd/popper.min.js"></script>
+<script src="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
