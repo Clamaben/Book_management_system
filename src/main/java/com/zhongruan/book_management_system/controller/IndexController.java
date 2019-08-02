2cd2a78e7e379ea1e4a59d970a50ffb7088ae88b
chenfeng11111 <1090268270@qq.com>
2019-08-02 15:57:06 +0800

    添加了注册的后台接口

--- src/main/java/com/zhongruan/book_management_system/controller/IndexController.java
+++ src/main/java/com/zhongruan/book_management_system/controller/IndexController.java
@@ -1,5 +1,9 @@
 package com.zhongruan.book_management_system.controller;
 
+import com.zhongruan.book_management_system.entity.User;
+import com.zhongruan.book_management_system.service.Userservice.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -8,22 +12,28 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @Controller
 public class IndexController {
+    @Autowired
+    @Qualifier("userService")
+    IUserService userService;
 
     //跳转登录界面
     @RequestMapping("toLogin")
-    public String toLogin(@RequestParam(value = "error",defaultValue = "false")String error,
+    public String toLogin(@RequestParam(value = "error", defaultValue = "false") String error,
                           Model model) {
-        if(error.equals("true")){
-            model.addAttribute("msg","用户名或密码错误");
+        if (error.equals("true")) {
+            model.addAttribute("msg", "用户名或密码错误");
         }
         return "login";
     }
@@ -36,5 +46,25 @@ public class IndexController {
     @RequestMapping("toIndex")
     public String toIndex() {
         return "index";
+    }
+
+    @RequestMapping("toRegister")
+    public String toRegister() {
+        return "register";
+    }
+
+    //注册接口，code 0失败，1成功，2用户名重复
+    @RequestMapping(value = "register",method = RequestMethod.POST)
+    @ResponseBody
+    public Map register(User user) {
+        Map<String, Object> map = new HashMap<>();
+        if(userService.getUserByname(user.getUsername())!=null){
+            map.put("code",2);
+            return map;
+        }
+        user.setRole("BORROWER");
+        int flag = userService.add(user);
+        map.put("code", flag);
+        return map;
     }
 }
